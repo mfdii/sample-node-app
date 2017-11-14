dcda0c4ee34d76850f28328fee802e6e098932df
Michael Ducy <michael@ducy.org>
2017-11-14 17:24:41 -0500

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -8,3 +8,14 @@ declare -A scaffolding_env
 # Define path to config file
 scaffolding_env[APP_CONFIG]="{{pkg.svc_config_path}}/config.json"
 
+do_begin() {
+set -x
+whoami
+
+}
+do_install(){
+return 0
+}
+do_build() {
+return 0
+}
